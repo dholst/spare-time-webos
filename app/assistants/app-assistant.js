40ea5500c78b091e94636137551cec83b1ae3cc9
Darrin Holst <darrinholst@gmail.com>
2010-09-12 15:05:04 -0500

    ability to add url on app launch

---
+++ app/assistants/app-assistant.js
@@ -0,0 +1,31 @@
+var SpareTime = SpareTime || {}
+SpareTime.MainStageName = "main"
+
+var AppAssistant = Class.create({
+  handleLaunch: function(launchParameters) {    
+    if(launchParameters && "add_url" == launchParameters.action) {
+      console.log("ADDING URL")
+      var credentials = new Credentials()
+    
+      if(credentials.username) {
+        Instapaper.credentials = credentials
+        new Instapaper().add(launchParameters.url, launchParameters.title, this.addSuccess.bind(this), this.addFailure.bind(this))
+      }
+      else {
+        this.showMessage("Must login using Spare Time first")
+      }
+    }
+  },
+
+  addSuccess: function() {
+    this.showMessage("URL added to Spare Time")
+  },
+
+  addFailure: function() {
+    this.showMessage("Unable to add URL to Spare Time")
+	},
+
+	showMessage: function(message) {
+	  Mojo.Controller.getAppController().showBanner(message, {source: "sparetime"})
+	}
+})
