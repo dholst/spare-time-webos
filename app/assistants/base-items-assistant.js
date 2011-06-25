c2c6875a33f54538250bb25f62cca32e7b65e3db
Darrin Holst <darrinholst@gmail.com>
2011-06-25 09:54:18 -0500

    semicolons

--- app/assistants/base-items-assistant.js
+++ app/assistants/base-items-assistant.js
@@ -1,101 +1,101 @@
 var BaseItemsAssistant = Class.create(BaseAssistant, {
   initialize: function($super) {
-    $super()
-    this.instapaper = new Instapaper()
-    this.items = {items: []}
-    this.allowItemDelete = true
-    this.commandMenuItems = [{}, {}, {label: "Refresh", icon: "refresh", command: "refresh"}]
+    $super();
+    this.instapaper = new Instapaper();
+    this.items = {items: []};
+    this.allowItemDelete = true;
+    this.commandMenuItems = [{}, {}, {label: "Refresh", icon: "refresh", command: "refresh"}];
   },
 
   setup: function($super) {
-    $super()
-    this.controller.setupWidget("items", {itemTemplate: 'items/item', onItemRendered: this.itemRendered.bind(this), swipeToDelete: this.allowItemDelete}, this.items)
-    this.controller.setupWidget(Mojo.Menu.commandMenu, {}, {items: this.commandMenuItems})
-    this.controller.listen("items", Mojo.Event.listTap, this.itemTapped = this.itemTapped.bind(this))
-    this.controller.listen("items", Mojo.Event.listDelete, this.itemDeleted = this.itemDeleted.bind(this))
-    this.controller.listen("switch", Mojo.Event.tap, this.swapScene = this.swapScene.bind(this))
-    this.controller.listen(document, SpareTime.Event.articleSaveStarting, this.itemSaveStarted = this.itemSaveStarted.bind(this))
-    this.controller.listen(document, SpareTime.Event.articleSaveComplete, this.itemSaveComplete = this.itemSaveComplete.bind(this))
-    this.controller.listen(document, SpareTime.Event.articleSaveFailed, this.itemSaveFailure = this.itemSaveFailure.bind(this))
+    $super();
+    this.controller.setupWidget("items", {itemTemplate: 'items/item', onItemRendered: this.itemRendered.bind(this), swipeToDelete: this.allowItemDelete}, this.items);
+    this.controller.setupWidget(Mojo.Menu.commandMenu, {}, {items: this.commandMenuItems});
+    this.controller.listen("items", Mojo.Event.listTap, this.itemTapped = this.itemTapped.bind(this));
+    this.controller.listen("items", Mojo.Event.listDelete, this.itemDeleted = this.itemDeleted.bind(this));
+    this.controller.listen("switch", Mojo.Event.tap, this.swapScene = this.swapScene.bind(this));
+    this.controller.listen(document, SpareTime.Event.articleSaveStarting, this.itemSaveStarted = this.itemSaveStarted.bind(this));
+    this.controller.listen(document, SpareTime.Event.articleSaveComplete, this.itemSaveComplete = this.itemSaveComplete.bind(this));
+    this.controller.listen(document, SpareTime.Event.articleSaveFailed, this.itemSaveFailure = this.itemSaveFailure.bind(this));
   },
 
   cleanup: function($super) {
-    $super()
-    this.controller.stopListening("items", Mojo.Event.listTap, this.itemTapped)
-    this.controller.stopListening("switch", Mojo.Event.tap, this.swapScene)
-    this.controller.stopListening(document, SpareTime.Event.articleSaveStarting, this.itemSaveStarted)
-    this.controller.stopListening(document, SpareTime.Event.articleSaveComplete, this.itemSaveComplete)
-    this.controller.stopListening(document, SpareTime.Event.articleSaveFailed, this.itemSaveFailure)
+    $super();
+    this.controller.stopListening("items", Mojo.Event.listTap, this.itemTapped);
+    this.controller.stopListening("switch", Mojo.Event.tap, this.swapScene);
+    this.controller.stopListening(document, SpareTime.Event.articleSaveStarting, this.itemSaveStarted);
+    this.controller.stopListening(document, SpareTime.Event.articleSaveComplete, this.itemSaveComplete);
+    this.controller.stopListening(document, SpareTime.Event.articleSaveFailed, this.itemSaveFailure);
   },
 
   ready: function($super) {
-    $super()
-    this.refresh()
+    $super();
+    this.refresh();
   },
 
   activate: function($super, itemToRemove) {
-    $super()
+    $super();
 
     if(itemToRemove) {
       for(var i = 0; i < this.items.items.length; i++) {
         if(this.items.items[i] == itemToRemove) {
-          this.items.items.splice(i, 1)
-          this.controller.modelChanged(this.items)
+          this.items.items.splice(i, 1);
+          this.controller.modelChanged(this.items);
         }
       }
     }
   },
 
   itemsRetrieved: function(items, folders) {
-    this.items.items.clear()
-    this.items.items.push.apply(this.items.items, items)
-    this.controller.modelChanged(this.items)
-    this.spinnerOff()
+    this.items.items.clear();
+    this.items.items.push.apply(this.items.items, items);
+    this.controller.modelChanged(this.items);
+    this.spinnerOff();
 
     if(folders) {
       folders.each(function(folder) {
-        this.otherScenes.push(folder)
-      }.bind(this))
+        this.otherScenes.push(folder);
+      }.bind(this));
     }
   },
 
   retrieveFailure: function() {
-    this.spinnerOff()
-    this.controller.stageController.pushScene("bail", "Unable to retrieve items")
+    this.spinnerOff();
+    this.controller.stageController.pushScene("bail", "Unable to retrieve items");
   },
 
   itemTapped: function(event) {
     if(event.originalEvent.target.hasClassName('star')) {
-      event.originalEvent.target.toggleClassName('on')
-      new Ajax.Request(event.item.starUrl, {method: 'get'})
+      event.originalEvent.target.toggleClassName('on');
+      var req = new Ajax.Request(event.item.starUrl, {method: 'get'});
     }
     else if(event.item.textUrl) {
-      this.controller.stageController.pushScene("text", event.item)
+      this.controller.stageController.pushScene("text", event.item);
     }
   },
 
   itemDeleted: function(event) {
     if(event.item.deleteUrl) {
-      new Ajax.Request(event.item.deleteUrl, {method: "post"})
+      var req = new Ajax.Request(event.item.deleteUrl, {method: "post"});
     }
   },
 
   handleCommand: function($super, event) {
     if("refresh" == event.command) {
-      this.refresh()
+      this.refresh();
     }
     else {
-      $super(event)
+      $super(event);
     }
   },
 
   swapScene: function() {
-    scenes = this.otherScenes
-    items = []
+    scenes = this.otherScenes;
+    items = [];
 
     scenes.each(function(scene) {
-      items.push({label: scene.name, command: scene.name})
-    })
+      items.push({label: scene.name, command: scene.name});
+    });
 
     this.controller.popupSubmenu({
       placeNear: $("switch"),
@@ -105,38 +105,40 @@ var BaseItemsAssistant = Class.create(BaseAssistant, {
         scenes.each(function(scene) {
           if(scene.name == command) {
             if(scene.url) {
-              this.controller.stageController.swapScene("folder", scene.name, scene.url)
+              this.controller.stageController.swapScene("folder", scene.name, scene.url);
             }
             else {
-              this.controller.stageController.swapScene(command.toLowerCase())
+              this.controller.stageController.swapScene(command.toLowerCase());
             }
           }
-        }.bind(this))
+        }.bind(this));
       }.bind(this)
-    })
+    });
   },
 
   refresh: function() {
-    this.firstTime = false
-    this.spinnerOn("retrieving articles...")
-    this.setOtherScenes()
-    this.retrieveItems(this.itemsRetrieved.bind(this), this.retrieveFailure.bind(this))
+    this.firstTime = false;
+    this.spinnerOn("retrieving articles...");
+    this.setOtherScenes();
+    this.retrieveItems(this.itemsRetrieved.bind(this), this.retrieveFailure.bind(this));
   },
 
   itemRendered: function(listWidget, item, node) {
-    ArticleSaver.isSaved(item.id, function() {node.down(".saved").show()})
+    ArticleSaver.isSaved(item.id, function() {
+      node.down(".saved").show();
+    });
   },
 
   itemSaveStarted: function() {
-    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saving").show()
+    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saving").show();
   },
 
   itemSaveComplete: function() {
-    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saving").hide()
-    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saved").show()
+    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saving").hide();
+    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saved").show();
   },
 
   itemSaveFailure: function() {
-    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saving").hide()
+    this.controller.sceneElement.querySelector("#item" + event.model.id + " .saving").hide();
   }
-})
+});
--- app/models/instapaper.js
+++ app/models/instapaper.js
@@ -1,22 +1,22 @@
 var Instapaper = Class.create({
   login: function(credentials, success, failure, offline) {
-    Instapaper.credentials = credentials
+    Instapaper.credentials = credentials;
 
-    new Ajax.Request("http://www.instapaper.com/user/login", {
+    var req = new Ajax.Request("http://www.instapaper.com/user/login", {
       method: "post",
       timeout: 5000,
       parameters: {username: credentials.username, password: credentials.password},
       onFailure: offline,
       onSuccess: this.loginComplete.bind(this, success, failure)
-    })
+    });
   },
 
   loginComplete: function(success, failure, response) {
     if(response.responseText.match(/form.*action="\/user\/login/)) {
-      failure()
+      failure();
     }
     else {
-      success()
+      success();
     }
   },
 
@@ -25,125 +25,124 @@ var Instapaper = Class.create({
       username: Instapaper.credentials.username,
       password: Instapaper.credentials.password,
       url: url
-    }
+    };
 
     if(title && title.strip().length) {
-      parameters.title = title
+      parameters.title = title;
     }
     else {
-      parameters['auto-title'] = 1
+      parameters['auto-title'] = 1;
     }
 
-    new Ajax.Request("http://www.instapaper.com/api/add", {
+    var req = new Ajax.Request("http://www.instapaper.com/api/add", {
       method: "post",
       parameters: parameters,
       onSuccess: success,
       onFailur: failure
-    })
+    });
   },
 
   getAllUnread: function(success, failure) {
-    new Ajax.Request("http://www.instapaper.com/u", {
+    var req = new Ajax.Request("http://www.instapaper.com/u", {
       method: "get",
       onSuccess: this.parseItems.bind(this, success),
       onFailure: failure
-    })
+    });
   },
 
   getArchived: function(success, failure) {
-    new Ajax.Request("http://www.instapaper.com/archive", {
+    var req = new Ajax.Request("http://www.instapaper.com/archive", {
       method: "get",
       onSuccess: this.parseItems.bind(this, success),
       onFailure: failure
-    })
+    });
   },
 
   getStarred: function(success, failure) {
-    new Ajax.Request("http://www.instapaper.com/starred", {
+    var req = new Ajax.Request("http://www.instapaper.com/starred", {
       method: "get",
       onSuccess: this.parseItems.bind(this, success),
       onFailure: failure
-    })
+    });
   },
 
   getFolder: function(url, success, failure) {
-    new Ajax.Request(url, {
+    var req = new Ajax.Request(url, {
       method: "get",
       onSuccess: this.parseItems.bind(this, success),
       onFailure: failure
-    })
+    });
   },
 
   absoluteUrl: function(a) {
-    return a ? a.href.replace(/file:\/\//, 'http://www.instapaper.com') : null
+    return a ? a.href.replace(/file:\/\//, 'http://www.instapaper.com') : null;
   },
 
   parseItems: function(success, response) {
-    var folders = []
-    var items = []
-    var div = document.createElement("div")
-    div.innerHTML = response.responseText.replace(/<img.*>/g, '')
+    var folders = [];
+    var items = [];
+    var div = document.createElement("div");
+    div.innerHTML = response.responseText.replace(/<img.*>/g, '');
 
     $(div).select("#bookmark_list .tableViewCell").each(function(rawItem) {
-      var item = {}
+      var item = {};
 
-      var title = rawItem.down("a.tableViewCellTitleLink")
+      var title = rawItem.down("a.tableViewCellTitleLink");
 
       if(title) {
-        item.id = rawItem.id.match(/\d+/)[0]
-        item.title = title ? title.innerHTML.unescapeHTML().replace(/&nbsp;/g, ' ') : ""
-        item.url = title ? title.href : null
+        item.id = rawItem.id.match(/\d+/)[0];
+        item.title = title ? title.innerHTML.unescapeHTML().replace(/&nbsp;/g, ' ') : "";
+        item.url = title ? title.href : null;
 
-        var host = rawItem.down("span.host")
-        item.host = host ? host.innerHTML.strip() : ""
+        var host = rawItem.down("span.host");
+        item.host = host ? host.innerHTML.strip() : "";
 
-        var textUrl = rawItem.down("a.textButton")
-        item.textUrl = "http://www.instapaper.com/m?u=" + escape(item.url)
+        var textUrl = rawItem.down("a.textButton");
+        item.textUrl = "http://www.instapaper.com/m?u=" + escape(item.url);
 
-        var deleteUrl = rawItem.down("a.deleteLink")
-        item.deleteUrl = this.absoluteUrl(deleteUrl)
+        var deleteUrl = rawItem.down("a.deleteLink");
+        item.deleteUrl = this.absoluteUrl(deleteUrl);
 
-        var archiveUrl = rawItem.down("a.archiveButton")
+        var archiveUrl = rawItem.down("a.archiveButton");
 
         if(archiveUrl && archiveUrl.innerHTML == "Delete") {
-          item.deleteUrl = this.absoluteUrl(archiveUrl)
+          item.deleteUrl = this.absoluteUrl(archiveUrl);
         }
         else if(archiveUrl) {
-          item.archiveUrl = this.absoluteUrl(archiveUrl)
+          item.archiveUrl = this.absoluteUrl(archiveUrl);
         }
 
-        var restoreUrl = rawItem.down("a.restoreButton")
-        item.restoreUrl = this.absoluteUrl(restoreUrl)
+        var restoreUrl = rawItem.down("a.restoreButton");
+        item.restoreUrl = this.absoluteUrl(restoreUrl);
 
-        var starUrl = rawItem.down("a.starToggleStarred")
-        item.starUrl = this.absoluteUrl(starUrl)
+        var starUrl = rawItem.down("a.starToggleStarred");
+        item.starUrl = this.absoluteUrl(starUrl);
 
         if(starUrl) {
-          item.starred = starUrl.style.display != 'none' ? 'on' : ''
+          item.starred = starUrl.style.display != 'none' ? 'on' : '';
         }
 
         rawItem.select(".moveTo").each(function(moveTo) {
-          item.moveTo = item.moveTo || []
-          var name = moveTo.innerHTML.strip()
+          item.moveTo = item.moveTo || [];
+          var name = moveTo.innerHTML.strip();
 
           if(name == "Read Later") {
-            name = "Unread"
+            name = "Unread";
           }
 
-          item.moveTo.push({url: this.absoluteUrl(moveTo), name: name})
-        }.bind(this))
+          item.moveTo.push({url: this.absoluteUrl(moveTo), name: name});
+        }.bind(this));
 
-        items.push(item)
-
+        items.push(item);
       }
-    }.bind(this))
+    }.bind(this));
 
     $(div).select("#folders a").each(function(folder) {
       if(folder.href.include("/folder/")) {
-        folders.push({name: folder.innerHTML.strip(), url: this.absoluteUrl(folder)})
+        folders.push({name: folder.innerHTML.strip(), url: this.absoluteUrl(folder)});
       }
-    }.bind(this))
+    }.bind(this));
 
-    success(items, folders)
+    success(items, folders);
   }
-})
+});
